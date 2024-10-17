4365ca135f419f13bf7e71c6b9e4e3d21782f776
Pooria-mh <pmah3000@gmail.com>
2024-10-17 12:59:21 +0330

    Print Grgroup names in console

--- init.c
+++ init.c
@@ -21,6 +21,7 @@ main(void)
 
   for(;;){
     printf(1, "init: starting sh\n");
+    printf(1, "Team Members : Alireza Karimi / Mohammad Taha Majlesi / Pooria Mahdian\n" );
     pid = fork();
     if(pid < 0){
       printf(1, "init: fork failed\n");
